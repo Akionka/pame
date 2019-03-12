681faf20b565214513b7e9e47c06a4c309c66329
Akionka <42269025+Akionka@users.noreply.github.com>
2019-03-12 23:31:55 +0300

    New update system

--- pame.lua
+++ pame.lua
@@ -1,8 +1,7 @@
 script_name('PAME')
 script_author('akionka')
-script_version('1.4')
-script_version_number(5)
-script_description([[Теперь вместо нагружающих 3D текстов с описанием персонажа у вас будет удобненький pame.]])
+script_version('1.5')
+script_version_number(6)
 
 local sampev = require 'lib.samp.events'
 local encoding = require 'encoding'
@@ -11,7 +10,7 @@ local dlstatus = require('moonloader').download_status
 encoding.default = 'cp1251'
 u8 = encoding.UTF8
 
-pames = {}
+local pames = {}
 
 local ini = inicfg.load({
 	settings = {
@@ -34,8 +33,19 @@ function main()
 	if not isSampfuncsLoaded() or not isSampLoaded() then return end
 	while not isSampAvailable() do wait(0) end
 	sampAddChatMessage(u8:decode("[PAME]: Скрипт {00FF00}успешно{FFFFFF} загружен. Версия: {2980b9}"..thisScript().version.."{FFFFFF}."), -1)
-	update()
-	while updateinprogess ~= false do wait(0) end
+
+	checkupdates('https://raw.githubusercontent.com/Akionka/pame/master/version.json')
+
+	sampRegisterChatCommand('pameupdate', function()
+		if updatesavaliable then
+			update('https://raw.githubusercontent.com/Akionka/pame/master/pame.lua')
+		end
+	end)
+
+	sampRegisterChatCommand('pamecheck', function()
+		checkupdates('https://raw.githubusercontent.com/Akionka/pame/master/version.json')
+	end)
+
 	sampRegisterChatCommand("pametog", function()
 		ini.settings.enable = not ini.settings.enable
 		inicfg.save(ini, "pame")
@@ -52,37 +62,43 @@ function main()
 	end)
 end
 
-function update()
-	local fpath = os.getenv('TEMP') .. '\\pame-version.json'
-	downloadUrlToFile('https://raw.githubusercontent.com/Akionka/pame/master/version.json', fpath, function(id, status, p1, p2)
-		if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-			local f = io.open(fpath, 'r')
-			if f then
-				local info = decodeJson(f:read('*a'))
-				if info and info.version then
-					version = info.version
-					version_num = info.version_num
-					if version_num > thisScript().version_num then
-						sampAddChatMessage(u8:decode("[PAME]: Найдено объявление. Текущая версия: {2980b9}"..thisScript().version.."{FFFFFF}, новая версия: {2980b9}"..version.."{FFFFFF}. Начинаю закачку."), -1)
-						lua_thread.create(goupdate)
+function checkupdates(json)
+	local fpath = os.getenv('TEMP')..'\\'..thisScript().name..'-version.json'
+	if doesFileExist(fpath) then os.remove(fpath) end
+	downloadUrlToFile(json, fpath, function(_, status, _, _)
+		if status == dlstatus.STATUSEX_ENDDOWNLOAD then
+			if doesFileExist(fpath) then
+				local f = io.open(fpath, 'r')
+				if f then
+					local info = decodeJson(f:read('*a'))
+					local updateversion = info.version_num
+					f:close()
+					os.remove(fpath)
+					if updateversion > thisScript().version_num then
+						updatesavaliable = true
+						sampAddChatMessage(u8:decode("[PAME]: Найдено объявление. Текущая версия: {2980b9}"..thisScript().version.."{FFFFFF}, новая версия: {2980b9}"..updateversion.."{FFFFFF}."), -1)
+						sampAddChatMessage(u8:decode("[PAME]: Используйте команду {2980b0}/pameupdate{FFFFFF}, чтобы обновиться до последней версии."), -1)
+						return true
 					else
+						updatesavaliable = false
 						sampAddChatMessage(u8:decode("[PAME]: У вас установлена самая свежая версия скрипта."), -1)
-						updateinprogess = false
 					end
+				else
+					updatesavaliable = false
+					sampAddChatMessage(u8:decode("[PAME]: Что-то пошло не так, упс. Попробуйте позже."), -1)
 				end
 			end
 		end
 	end)
 end
 
-function goupdate()
-	wait(300)
-	downloadUrlToFile("https://raw.githubusercontent.com/Akionka/pame/master/pame.lua", thisScript().path, function(id3, status1, p13, p23)
+function update(url)
+	downloadUrlToFile(url, thisScript().path, function(_, status1, _, _)
 		if status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
 			sampAddChatMessage(u8:decode('[PAME]: Новая версия установлена! Чтобы скрипт обновился нужно либо перезайти в игру, либо ...'), -1)
-			sampAddChatMessage(u8:decode('[PAME]: ... если у вас есть автоперезагрузка скриптов, то новая версия уже готова и снизу вы увидите приветственное сообщение'), -1)
+			sampAddChatMessage(u8:decode('[PAME]: ... если у вас есть автоперезагрузка скриптов, то новая версия уже готова и снизу вы увидите приветственное сообщение.'), -1)
 			sampAddChatMessage(u8:decode('[PAME]: Если что-то пошло не так, то сообщите мне об этом в VK или Telegram > {2980b0}vk.com/akionka teleg.run/akionka{FFFFFF}.'), -1)
-			updateinprogess = false
+			thisScript():reload()
 		end
 	end)
 end
